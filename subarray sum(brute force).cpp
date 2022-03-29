206e95c82b81d54004827a4eec119b963d534ec9
SAI HEMANTH BOLLA <71115725+Sai1166@users.noreply.github.com>
2022-03-29 05:30:45 -0700

    Update subarray sum(brute force).cpp

--- subarray sum(brute force).cpp
+++ subarray sum(brute force).cpp
@@ -5,7 +5,7 @@ int main()
     int n;
     cin>>n;
     int a[n];
-    sum=0;
+    int sum=0;
     for(int i=0;i<n;i++)
     {
         cin>>a[i];
@@ -13,7 +13,7 @@ int main()
     for(int i=0;i<n;i++)  // two pointers are intiated i,j and k for printing array item.
     {
         for(int j=i+1;j<=n;j++)
-        {
+        {   sum=0;
             for(int k=i;k<j;k++)
             {
                 sum=sum+a[k]
